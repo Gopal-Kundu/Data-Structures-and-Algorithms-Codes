c790b6e9f4809df18de88381cb99d946f20d54b2
Gopal Kundu <kundugopal2005@gmail.com>
2025-09-27 11:59:51 +0530

    Longest Well-Performing Interval added

---
+++ Random Questions/Longest Well-Performing Interval LC-1124.java
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/longest-well-performing-interval/description/
+
+//Brute Foce
+class Solution {
+    public int longestWPI(int[] hours) {
+        int maxLen = 0;
+        for (int i = 0; i < hours.length; i++) {
+            int T = 0, N = 0;
+            for (int j = i; j < hours.length; j++) {
+                if (hours[j] > 8) T++;
+                else N++;
+                if (T > N) {
+                    maxLen = Math.max(j - i + 1, maxLen);
+                }
+
+            }
+        }
+        return maxLen;
+    }
+}
