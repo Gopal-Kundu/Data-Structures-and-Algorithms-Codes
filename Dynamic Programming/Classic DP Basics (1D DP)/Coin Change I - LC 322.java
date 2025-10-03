f4ec16fa9afeb87a7c727c67680236e801f21556
Gopal-Kundu <kundugopal2005@gmail.com>
2025-10-03 06:15:54 +0530

    Coin Change I added

---
+++ Dynamic Programming/Classic DP Basics (1D DP)/Coin Change I - LC 322.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/house-robber-ii/description/
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int minCoinsReq[] = new int[amount+1];
+        for(int i = 1; i <= amount; i++){
+            minCoinsReq[i] = Integer.MAX_VALUE;
+            for(int coin : coins){
+                if(coin <= i && minCoinsReq[i - coin] != Integer.MAX_VALUE )
+                minCoinsReq[i] = Math.min(minCoinsReq[i], 1 + minCoinsReq[i - coin]);
+            }
+        }
+        return minCoinsReq[amount] != Integer.MAX_VALUE ? minCoinsReq[amount] : -1;
+    }
+}
