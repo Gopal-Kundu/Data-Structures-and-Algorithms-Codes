7923db4de1f7737baa7efa4b1db0f48e311f216d
Gopal Kundu <kundugopal2005@gmail.com>
2026-08-13 11:40:52 +0530

    Boats to Save People added

---
+++ Greedy/Boats to Save People.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/boats-to-save-people/
+
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length-1;
+        int boats = 0;
+        while(i <= j){
+            if(people[i] + people[j] <= limit){
+                boats++;
+                i++;
+                j--;
+            }else{
+                j--;
+                boats++;
+            }
+        }
+        return boats;
+    }
+}
