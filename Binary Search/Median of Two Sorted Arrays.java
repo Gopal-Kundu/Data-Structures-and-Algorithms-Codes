b4ef8e11e87654389748cafc402373ae55131b5d
Gopal Kundu <kundugopal2005@gmail.com>
2026-09-03 07:28:56 +0530

    Median of Two Sorted Arrays added

---
+++ Binary Search/Median of Two Sorted Arrays.java
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length, m = nums2.length;
+        if(n > m) return findMedianSortedArrays(nums2, nums1);
+        int left = (n + m + 1) / 2;
+        int low = 0, high = nums1.length;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 2;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = l1;
+            int r1 = Integer.MAX_VALUE, r2 = r1;
+            if (mid1 < n)
+                r1 = nums1[mid1];
+            if (mid2 < m)
+                r2 = nums2[mid2];
+            if (mid1 - 1 >= 0)
+                l1 = nums1[mid1 - 1];
+            if (mid2 - 1 >= 0)
+                l2 = nums2[mid2 - 1];
+
+            if (l1 > r2)
+                high = mid1 - 1;
+            else if (l2 > r1)
+                low = mid1 + 1;
+
+            else if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 0)
+                    return (double) (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                else
+                    return (double) Math.max(l1, l2);
+            }
+        }
+        return (double) 0;
+    }
+}
