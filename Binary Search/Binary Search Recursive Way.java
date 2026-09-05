3ca05c889b2955fea8279d48161a17cefed546c9
Gopal Kundu <kundugopal2005@gmail.com>
2026-09-05 07:19:04 +0530

    Binary Search Recursive Way added

---
+++ Binary Search/Binary Search Recursive Way.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/binary-search/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length-1);
+    }
+
+    int binarySearch(int[] nums, int target, int low, int high){
+        if(low > high) return -1;
+        int mid = low + (high - low)/2;
+        if(nums[mid] == target) return mid;
+        if(nums[mid] > target){
+            return binarySearch(nums, target, low, mid-1);
+        }else{
+            return binarySearch(nums, target, mid+1, high);
+        }
+    }
+}
